8cbbf4ef776610c5355edb0deb836c02bcf5246f
David Horès <hores.d@gmail.com>
2025-09-04 15:53:45 +0200

    Add incrementViews and updateViews methods to ArticleController

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -31,6 +31,9 @@ class ArticleController
             throw new Exception("L'article demandé n'existe pas.");
         }
 
+        $article->incrementViews();
+        $articleManager->updateViews($article);
+
         $commentManager = new CommentManager();
         $comments = $commentManager->getAllCommentsByArticleId($id);
 
